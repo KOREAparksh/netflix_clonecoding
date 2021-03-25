52645eb6c296071c9976d2140bcb5e32fb1ee004
KOREAparksh <45682868+KOREAparksh@users.noreply.github.com>
2021-03-25 15:52:10 +0900

    Initial commit

---
+++ android/app/src/main/java/com/clonecoding/netflix_clonecoding/MainActivity.java
@@ -0,0 +1,6 @@
+package com.clonecoding.netflix_clonecoding;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:netflix_clonecoding/widget/bottom_bar.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TabController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Seungflix',
+      theme: ThemeData(
+          brightness: Brightness.dark,
+          primaryColor: Colors.black,
+          accentColor: Colors.white),
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          body: TabBarView(
+            physics: NeverScrollableScrollPhysics(), // 손가락으로 탭 전환 막기
+            children: <Widget>[
+              Container(
+                child: Center(
+                  child: Text('home'),
+                ),
+              ),
+              Container(
+                child: Center(
+                  child: Text('search'),
+                ),
+              ),
+              Container(
+                child: Center(
+                  child: Text('save'),
+                ),
+              ),
+              Container(
+                child: Center(
+                  child: Text('more'),
+                ),
+              ),
+            ],
+          ),
+          bottomNavigationBar: Bottom(),
+        ),
+      ),
+    );
+  }
+}
